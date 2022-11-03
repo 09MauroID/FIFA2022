18967e5f79b3fc2ad8f44f60b2b4f8988deb391f
Lucia-Barcenas <lucia.barcenaset12d1@gmail.com>
2022-11-03 16:16:03 -0300

    Update HabilidadTest.cs

--- src/Fifa.AdoEt12.Test/HabilidadTest.cs
+++ src/Fifa.AdoEt12.Test/HabilidadTest.cs
@@ -1,10 +1,11 @@
 using et12.edu.ar.AGBD.Ado;
+using Fifa.Core;
 
 namespace Fifa.AdoEt12.Test;
 
 public class HabilidadTest
 {
-    public AdoFifa Ado { get; set; } 
+    public AdoFifa Ado { get; set; }
     public HabilidadTest()
     {
         var adoAGBD = FactoryAdoAGBD.GetAdoMySQL("appSettings.json", "test");
@@ -12,8 +13,8 @@ public class HabilidadTest
     }
 
     [Fact]
-    public void Test1()
+    public void AltaHabilidad()
     {
-        
+        var habilidad = new Habilidad("");
     }
 }
