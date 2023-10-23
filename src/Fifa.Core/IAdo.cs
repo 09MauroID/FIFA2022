e98b9e36f80f71321c54802e8361e40c17c3c2a0
09MauroID <mauro.soto.et12@gmail.com>
2023-10-23 17:14:29 -0300

    Inicializacion de la pag 23/10

--- src/Fifa.Core/IAdo.cs
+++ src/Fifa.Core/IAdo.cs
@@ -6,19 +6,21 @@ public interface IAdo
     Task AltaHabilidadAsync(Habilidad habilidad);
     public List<Habilidad> ObtenerHabilidades();
     public Task<List<Habilidad>> ObtenerHabilidadesAsync();
-    public void AltaTransferencia(Transferencia transferencia);
-    Task AltaTransferenciaAsync(Transferencia transferencia);
-    public List<Usuario> ObtenerUsuarios();
-    public void ObtenerPosiciones(Posicion posicion);
-    public Task<List<Posicion>> ObtenerPosicionesASync();
-    public void ObtenerFutbolistas(Futbolista futbolista);
-    public Task<List<Futbolista>> ObtenerFutbolistasAync();
-    public void AltaUsuario(Usuario usuario);
-    Task AltaUsuarioAsync(Usuario Usuario);
-    public void AltaFutbolista(Futbolista futbolista);
-    Task AltaFutbolistaAsync(Futbolista futbolista);
+    public void AltaPosicion(Posicion posicion);
+    Task AltaPosicionAsync(Posicion posicion);
     public List<Posicion> ObtenerPosiciones();
     public Task<List<Posicion>> ObtenerPosicionesAsync();
+    public void AltaUsuario(Usuario usuario);
+    Task AltaUsuarioAsync(Usuario Usuario);
+    public List<Usuario> ObtenerUsuarios();
+    public Task<List<Usuario>> ObtenerUsuariosAsync();
+    public void AltaFutbolista(Futbolista futbolista);
+    Task AltaFutbolistaAsync(Futbolista futbolista);
+    public List<Futbolista> ObtenerFutbolistas();
+    public Task<List<Futbolista>> ObtenerFutbolistasAsync();
+    public void AltaTransferencia(Transferencia transferencia);
+    Task AltaTransferenciaAsync(Transferencia transferencia);
     public List<Transferencia> TransferenciasActivas();
+
 
 }
