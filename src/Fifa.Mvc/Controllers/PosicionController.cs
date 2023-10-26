66d5c4406c80e5563a01205cfba5ee32ac99ae40
09MauroID <mauro.soto.et12@gmail.com>
2023-10-26 15:48:23 -0300

    26/10 posicion

---
+++ src/Fifa.Mvc/Controllers/PosicionController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fifa.Mvc.Controllers;
+public class PosicionController : Controller
+{
+    public PosicionController(ILogger<PosicionController> logger)
+    {
+    
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpGet]
+    public IActionResult Alta() => ViewComponent("AltaPosicion");
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View("Error!");
+    }
+}
